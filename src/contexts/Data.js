40c7126dd50b2f81b881553b6f48e706c1af0c4a
Andryel Blanco <andryelmblanco@gmail.com>
2021-11-13 08:45:34 -0300

    Solving Api Bugs 5

--- src/contexts/Data.js
+++ src/contexts/Data.js
@@ -3,11 +3,10 @@ import React from 'react';
 export const DataContext = React.createContext({});
 
 export const DataProvider = ({children}) => {
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    const URL_BASE = `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+    // const API_KEY = process.env.REACT_APP_API_KEY;
+    const URL_BASE = `http://api.themoviedb.org/3/discover/movie?api_key=beff5fb624944d0d092f39c19644507d`;
     const [movieData, setMovieData] = React.useState(null);
     const [currentURL, setCurrentURL] = React.useState(``);
-    const [json, setJson] = React.useState(false);
 
     const [error, setError] = React.useState(null);
     const [loading, setLoading] = React.useState(false);
@@ -25,16 +24,14 @@ export const DataProvider = ({children}) => {
 
             response = await fetch(URL_BASE + URL_PAGE + URL_COMPLEMENT);
             json = await response.json();
-            let {results} = await json;
-            setJson(results)
             
             if (response.success === false) throw new Error(json.status_message);
         } catch(err) {
             json = null;
             setError(err);
         } finally{
-            let randomIndex = Math.floor(Math.random() * (5));
-            let {original_title, title, overview, poster_path, release_date, vote_average, backdrop_path} = json[randomIndex];
+            let {results} = await json;
+            let {original_title, title, overview, poster_path, release_date, vote_average, backdrop_path} = results[Math.floor(Math.random() * (5))];
             setMovieData({
                 original_title: original_title,
                 overview: overview,
